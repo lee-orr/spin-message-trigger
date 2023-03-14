cb6da413ba76167faaeb95a579dd4ad526f532bd
lee-orr <lee-orr@users.noreply.github.com>
2023-03-14 19:19:35 +0000

    clippy and start setting gateway

--- spin-message-trigger/src/main.rs
+++ spin-message-trigger/src/main.rs
@@ -1,18 +1,19 @@
 mod broker;
 mod in_memory_broker;
 mod wit;
+mod gateway;
 
-use anyhow::{Error, bail};
+use anyhow::{bail, Error};
 use broker::MessageBroker;
 use clap::Parser;
 use serde::{Deserialize, Serialize};
 use spin_trigger::{cli::TriggerExecutorCommand, TriggerAppEngine, TriggerExecutor};
-use std::{collections::HashMap, default};
+use std::collections::HashMap;
 
 use in_memory_broker::InMemoryBroker;
 use wit::{
-    messages::{MessageParam, MetadataParam, SubjectMessageParam, Outcome},
-    SubjectMessage, Message,
+    messages::{MessageParam, MetadataParam, Outcome, SubjectMessageParam},
+    SubjectMessage,
 };
 
 #[tokio::main]
@@ -35,8 +36,13 @@ pub enum BrokerConfig {
 }
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
-pub struct GatewayConfig {
-
+pub enum GatewayConfig {
+    #[default]
+    None,
+    Http {
+        port: u16,
+        websockets: bool,
+    },
 }
 
 struct MessageTrigger {
@@ -138,17 +144,27 @@ impl TriggerExecutor for MessageTrigger {
 }
 
 impl MessageTrigger {
-    async fn send_with_broker(&self, broker: &String, subject: &String, mut msg: SubjectMessage) -> anyhow::Result<()>{
-        let broker = msg.broker.as_ref().unwrap_or(broker);
+    async fn send_with_broker(
+        &self,
+        broker: &str,
+        subject: &str,
+        mut msg: SubjectMessage,
+    ) -> anyhow::Result<()> {
+        let broker = msg.broker.as_deref().unwrap_or(broker);
         if let Some(broker) = self.brokers.get(broker) {
-            msg.subject = Some(msg.subject.unwrap_or(subject.clone()));
+            msg.subject = Some(msg.subject.unwrap_or(subject.to_string()));
             broker.publish(msg).await?;
             Ok(())
         } else {
             bail!("No such broker");
         }
     }
-    async fn send_all_with_broker(&self, broker: &String, subject: &String, msgs: Vec<SubjectMessage>) -> anyhow::Result<()>{
+    async fn send_all_with_broker(
+        &self,
+        broker: &str,
+        subject: &str,
+        msgs: Vec<SubjectMessage>,
+    ) -> anyhow::Result<()> {
         for msg in msgs.into_iter() {
             self.send_with_broker(broker, subject, msg).await?;
         }
@@ -181,18 +197,28 @@ impl MessageTrigger {
                 body: message.message.body.as_deref(),
                 metadata: metadata.as_slice(),
             },
-            broker: Some(&config.broker)
+            broker: Some(&config.broker),
         };
 
         let result = engine.handle_message(&mut store, message).await?;
 
         match (result, &config.result) {
-            (Outcome::Publish(msgs), MessageResultType::Publish { default_broker, default_subject }) => self.send_all_with_broker(default_broker, default_subject, msgs).await?,
-            (Outcome::Publish(msgs), MessageResultType::None ) => {
+            (
+                Outcome::Publish(msgs),
+                MessageResultType::Publish {
+                    default_broker,
+                    default_subject,
+                },
+            ) => {
+                self.send_all_with_broker(default_broker, default_subject, msgs)
+                    .await?
+            }
+            (Outcome::Publish(msgs), MessageResultType::None) => {
                 if let Some(default_subject) = original_subject {
-                    self.send_all_with_broker(&config.broker, default_subject, msgs).await?
+                    self.send_all_with_broker(&config.broker, default_subject, msgs)
+                        .await?
                 }
-            },
+            }
             _ => {}
         }
         Ok(())
