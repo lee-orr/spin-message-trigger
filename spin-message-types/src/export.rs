98428ff7bd5c3253f8a23f38a4eb05d1ce727e8a
lee-orr <lee-orr@users.noreply.github.com>
2023-05-04 19:00:01 +0000

    fix tests

--- spin-message-types/src/export.rs
+++ spin-message-types/src/export.rs
@@ -5,7 +5,7 @@ wasmtime::component::bindgen!({
     async: true
 });
 
-pub use spin_message_types::{InternalMessage, InternalOutputMessage, Outcome};
+pub use self::spin_message_types::{InternalMessage, InternalOutputMessage, Outcome};
 
 impl From<InternalOutputMessage> for OutputMessage {
     fn from(value: InternalOutputMessage) -> Self {
