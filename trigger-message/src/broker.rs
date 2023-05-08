f6a74c0b73970e3e4aaef602908167b6545c230e
lee-orr <lee-orr@users.noreply.github.com>
2023-05-08 17:16:53 +0000

    move request response implementation into broker

--- trigger-message/src/broker.rs
+++ trigger-message/src/broker.rs
@@ -1,8 +1,11 @@
-use anyhow::Result;
+use anyhow::{Result, bail};
 use async_trait::async_trait;
+use serde::Serialize;
 use tokio::sync::broadcast;
 
-use spin_message_types::{InputMessage, OutputMessage};
+use spin_message_types::{InputMessage, OutputMessage, HttpRequest, HttpResponse};
+
+use crate::configs::GatewayRequestResponseConfig;
 
 pub type Receiver = broadcast::Receiver<InputMessage>;
 pub type Sender = broadcast::Sender<InputMessage>;
@@ -23,5 +26,62 @@ pub trait MessageBroker: Send + Sync {
             self.publish(msg).await?;
         }
         Ok(())
+    }
+
+    async fn request(&self, request: HttpRequest, serializer: &GatewayRequestResponseConfig) -> Result<HttpResponse> {
+
+        let request_id = ulid::Ulid::new();
+        let path = &request.path;
+        let method = &request.method;
+        let subject_base = format!("{request_id}.{method}.{path}");
+        let subject = format!("request.{subject_base}");
+        let response_subject = format!("response.{subject_base}");
+
+        let body = match serializer {
+            GatewayRequestResponseConfig::Messagepack => {
+                let mut buf = Vec::new();
+                if let Ok(()) = request.serialize(&mut rmp_serde::Serializer::new(&mut buf)) {
+                    Some(buf)
+                } else {
+                    None
+                }
+            }
+            GatewayRequestResponseConfig::Json => {
+                serde_json::to_string(&request).ok().map(|v| v.into_bytes())
+            }
+        };
+
+        let (Some(body), Ok(mut subscribe)) = (body, self.subscribe(&response_subject).await) else {
+            bail!("Couldn't Subscribe");
+        };
+
+        self
+            .publish(OutputMessage {
+                subject: Some(subject),
+                message: body,
+                broker: None,
+            })
+            .await?;
+
+        let Ok(result) = subscribe.recv().await else {
+            bail!("couldn't get result");
+        };
+
+        
+        println!("Got Response: {result:?}");
+
+        let result = match serializer {
+            GatewayRequestResponseConfig::Messagepack => {
+                rmp_serde::from_slice::<HttpResponse>(&result.message).ok()
+            }
+            GatewayRequestResponseConfig::Json => {
+                serde_json::from_slice::<HttpResponse>(&result.message).ok()
+            }
+        };
+        if let Some(result) = result {
+            Ok(result)
+        } else {
+            bail!("couldn't process result")
+        }
     }
 }
